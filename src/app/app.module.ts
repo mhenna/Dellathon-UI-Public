178fc17ad2bcb18a6235c5fa4fdac90f52a04125
hassay3 <youssef.elhady@dell.com>
2019-08-29 13:25:25 +0200

    added photo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,40 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgZorroAntdModule, NZ_I18N, en_US, zh_CN} from 'ng-zorro-antd';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import { FooterComponent } from './footer/footer.component';
+import { CommonModule } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { HttpModule } from '@angular/http';
+
+
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    NgZorroAntdModule,
+    ReactiveFormsModule,
+    CommonModule,
+    HttpModule,
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
